dbe1dbac73312078e976be1fb12cb179e3e0afa3
Jason Lengstorf <jason@lengstorf.com>
2019-08-20 10:07:36 -0700

    feat: read data from Stripe

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config()
 
 module.exports = {
-  /* Your site config here */
+  plugins: [],
 }
